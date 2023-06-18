753785edb92592dcdf0b49e51badc3f7fa16b4a8
Yuya Tsugane <gold.corage.121@gmail.com>
2023-06-18 19:35:59 +0900

    ログイン画面の仮作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,23 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        "primary": "#fef3c7",
+        "secondary": "#0891b2",
+        "accent": "#155e75",
+        "neutral": "#2b3440",
+        "base-100": "#ffffff",
+        "info": "#3abff8",
+        "success": "#36d399",
+        "warning": "#fbbd23",
+        "error": "#f87272",
+        },
+      },
+    ],
+  },
   plugins: [
     require('daisyui')
   ]
