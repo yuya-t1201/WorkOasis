a71dd198b8e5f3fb461a48a9d4a026749f5ddb3f
Yuya Tsugane <gold.corage.121@gmail.com>
2023-06-21 20:23:27 +0900

    ヘッダー作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,19 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ['Noto Serif JP', 'selif']
+      }
+    }
+  },
   daisyui: {
     themes: [
       {
         mytheme: {
         "primary": "#fef3c7",
-        "secondary": "#0891b2",
+        "secondary": "#607EAA",
         "accent": "#155e75",
         "neutral": "#2b3440",
         "base-100": "#ffffff",
