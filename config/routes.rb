98b54262f5d6d6a6249997de6185f849c3d91b7e
Yuya Tsugane <gold.corage.121@gmail.com>
2023-06-25 15:01:00 +0900

    ユーザー登録機能実装、ヘッダーのリンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[new create]
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  delete '/logout',  to: 'sessions#destroy', as: 'logout'
   post "oauth/callback", to: "oauths#callback"
   get "oauth/callback", to: "oauths#callback"
   get "users/auth/google/callback", to: "oauths#callback"
