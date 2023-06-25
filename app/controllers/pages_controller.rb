b1478929a8e93219b967099ebe4ac863e65b7c1b
Yuya Tsugane <gold.corage.121@gmail.com>
2023-06-25 23:59:19 +0900

    LPページ作成

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def lp
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :workspaces
   # Defines the root path route ("/")
-
+  root 'pages#lp'
 
 end
