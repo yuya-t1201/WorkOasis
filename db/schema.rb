94a95e3d489587ac46b9403485295b7687047d7d
Yuya Tsugane <gold.corage.121@gmail.com>
2023-09-08 19:23:29 +0900

    マイページの作成、デフォルト画像の設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_31_110821) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_08_101227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_110821) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
