20de391c88c5acc7ec8788f6616c9d329af11b62
Yuya Tsugane <gold.corage.121@gmail.com>
2023-09-22 01:01:26 +0900

    評価画面のUI作成

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -11,9 +11,9 @@ export { application }
 + import jquery from "jquery"
 + window.$ = jquery
 
-import Raty from "raty.js"
-window.raty = function (elem, opt) {
-    let raty = new Raty(elem, opt)
-    raty.init();
-    return raty;
-}
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import '@fortawesome/fontawesome-free'
+library.add(fas, far, fab)
