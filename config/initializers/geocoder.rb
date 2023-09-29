02c20735a5e8f342ee401aade3a726382630bffe
Yuya Tsugane <gold.corage.121@gmail.com>
2023-09-29 23:21:10 +0900

    geocoder.rb削除

--- config/initializers/geocoder.rb
+++
@@ -1,6 +0,0 @@
-Geocoder.configure(
-  lookup: :google,
-  api_key: ENV['GOOGLE_MAPS_API_KEY'],
-  use_https: true,
-  units: :km
-)
--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -80,7 +80,7 @@ Rails.application.config.sorcery.configure do |config|
   # i.e. [:twitter, :facebook, :github, :linkedin, :xing, :google, :liveid, :salesforce, :slack, :line].
   # Default: `[]`
   #
-  config.external_providers = %i[google]
+  # config.external_providers = %i[google]
 
   # You can change it by your local ca_file. i.e. '/etc/pki/tls/certs/ca-bundle.crt'
   # Path to ca_file. By default use a internal ca-bundle.crt.
@@ -158,9 +158,9 @@ Rails.application.config.sorcery.configure do |config|
   # config.auth0.callback_url = "https://0.0.0.0:3000/oauth/callback?provider=auth0"
   # config.auth0.site = "https://example.auth0.com"
   #
-  config.google.key = Rails.application.credentials.dig(:google, :client_id)
-  config.google.secret = Rails.application.credentials.dig(:google, :client_secret)
-  config.google.callback_url = Settings.sorcery[:google_callback_url]
+  #config.google.key = Rails.application.credentials.dig(:google, :client_id)
+  #config.google.secret = Rails.application.credentials.dig(:google, :client_secret)
+  #config.google.callback_url = Settings.sorcery[:google_callback_url]
   # config.google.user_info_mapping = {:email => "email", :name => "name" }
   # config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
   #
