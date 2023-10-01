24272fce4d7924341d51c080c93e5ea2d71a1752
Yuya Tsugane <gold.corage.121@gmail.com>
2023-10-01 15:00:54 +0900

    apprication.jsの場所変更

---
+++ app/javascript/apprication.js
@@ -0,0 +1,4 @@
+import "@hotwired/turbo-rails"
+import "controllers"
+import jquery from "jquery"
+window.$ = jquery
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,13 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-+ import jquery from "jquery"
-+ window.$ = jquery
-
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { library } from "@fortawesome/fontawesome-svg-core";
-import '@fortawesome/fontawesome-free'
-library.add(fas, far, fab)
