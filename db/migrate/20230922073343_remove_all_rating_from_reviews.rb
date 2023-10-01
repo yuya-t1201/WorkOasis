45fc325402caa52b18f475566e9d0f79711dced7
Yuya Tsugane <gold.corage.121@gmail.com>
2023-10-01 20:07:33 +0900

    マイグレーションファイル修正

--- db/migrate/20230922073343_remove_all_rating_from_reviews.rb
+++ db/migrate/20230922073343_remove_all_rating_from_reviews.rb
@@ -1,9 +1,9 @@
 class RemoveAllRatingFromReviews < ActiveRecord::Migration[7.0]
   def up
-    remove_column :reviews, :all_rating
+    # remove_column :reviews, :all_rating
   end
 
   def down
-    add_column :reviews, :all_rating, :float
+    # add_column :reviews, :all_rating, :float
   end
 end
