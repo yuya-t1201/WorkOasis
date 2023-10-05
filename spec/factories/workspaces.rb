d711fdb82625fc820aeec5f2ae9391e012e6d63d
Yuya Tsugane <gold.corage.121@gmail.com>
2023-10-05 20:09:57 +0900

    workspaceモデルにバリデーション追加

--- spec/factories/workspaces.rb
+++ spec/factories/workspaces.rb
@@ -13,11 +13,13 @@
 #  workspace_image :string
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
+#  user_id         :integer
 #
 # Indexes
 #
 #  index_workspaces_on_address  (address) UNIQUE
 #  index_workspaces_on_title    (title) UNIQUE
+#  index_workspaces_on_user_id  (user_id)
 #
 FactoryBot.define do
   factory :workspace do
