553c251cdc16c134107ba816a75594a97fae0f40
Yuya Tsugane <gold.corage.121@gmail.com>
2023-11-03 16:47:00 +0900

    オートコンプリート機能追加

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,6 @@
 import { Application } from "@hotwired/stimulus"
 import { Autocomplete } from "stimulus-autocomplete"
-import "./controllers"
-import './channels/workspace_notifications_channel.js'
+
 import jquery from "jquery"
 window.$ = jquery
 
