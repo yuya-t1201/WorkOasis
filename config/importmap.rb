385a5e5c512e0edb8f4b28c860795fc741d6480b
Yuya Tsugane <gold.corage.121@gmail.com>
2023-11-08 11:55:46 +0900

    fontawesomeの記述変更「

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,6 @@ pin "@hotwired/stimulus", to: 'stimulus.min.js', preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js"
-pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.4.2/js/all.js"
 pin_all_from "app/javascript/channels", under: "channels"
 pin "stimulus-autocomplete", to: "https://ga.jspm.io/npm:stimulus-autocomplete@3.1.0/src/autocomplete.js"
 pin "@github/auto-complete-element", to: "https://ga.jspm.io/npm:@github/auto-complete-element@3.5.1/dist/index.js"
